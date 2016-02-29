6452f1e7077d68b3139569460a8cfb8c6f0b65ad
OscarDuranX <oscarduran@iesebre.com>
2016-02-29 13:30:40 +0100

    update 5.2

--- config/app.php
+++ config/app.php
@@ -14,6 +14,7 @@ return [
     */
 
     'debug' => env('APP_DEBUG', false),
+    'env' => env('APP_ENV', 'production'),
 
     /*
     |--------------------------------------------------------------------------
@@ -119,7 +120,6 @@ return [
         Illuminate\Bus\BusServiceProvider::class,
         Illuminate\Cache\CacheServiceProvider::class,
         Illuminate\Foundation\Providers\ConsoleSupportServiceProvider::class,
-        Illuminate\Routing\ControllerServiceProvider::class,
         Illuminate\Cookie\CookieServiceProvider::class,
         Illuminate\Database\DatabaseServiceProvider::class,
         Illuminate\Encryption\EncryptionServiceProvider::class,
